298f7fc65a1b0ac6fcf9316065ab7fc115a6f2aa
LB165 <98264741+LB165@users.noreply.github.com>
2022-08-21 19:20:51 +0200

    added Form

--- src/app.js
+++ src/app.js
@@ -41,9 +41,18 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-let city = "Zagreb";
-let apiKey = "09b62f5636602cc015e9862bc2ce7fa3";
-let units = "metric";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "09b62f5636602cc015e9862bc2ce7fa3";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+search("New York");
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
